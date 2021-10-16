d27d577e09db5075c30e81080dac7a965e8036be
Gabriel Tinoco <gabrielslai@gmail.com>
2021-10-16 17:23:27 -0300

    Added mapping for database objects.

---
+++ server/models/contact.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose');
+
+let contactModel = mongoose.Schema({
+    name: String,
+    email: String,
+    number: String
+},
+{
+    collection: "contacts"
+});
+
+module.exports = mongoose.model('contact', contactModel);
