53dc3fc3d4b32485662b655fe1ff7c5c365f62e6
Gabriel Tinoco <gabrielslai@gmail.com>
2021-10-17 12:41:37 -0300

    Implemented controllers for displaying and processing the contact update page.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -19,7 +19,40 @@ module.exports.displayBusinessContactsPage = (req, res, next) => {
   });
 };
 
+/* GET updateBusinessContact page. */
 module.exports.displayUpdatePage = (req, res, next) => {
-  res.render('updateBusinessContact', {title: 'Update', contactList: contactList})  
+  let id = req.params.id;
+  contact.findById(id, (err, contactToUpdate) => {
+    if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+          res.render('updateBusinessContact', { title: 'Update', contact: contactToUpdate });        
+        }
+  });
 };
 
+module.exports.processUpdatePage = (req, res, next) => {
+  let id = req.params.id;
+
+  let updatedContact = contact({
+    "_id": id,
+    "name": req.body.contactName,
+    "email": req.body.contactEmail,
+    "number": req.body.contactNumber
+  });
+
+  contact.updateOne({_id: id}, updatedContact, (err) => {
+    if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+          res.redirect("/users/business-contacts")           ;
+        }
+  });
+};
