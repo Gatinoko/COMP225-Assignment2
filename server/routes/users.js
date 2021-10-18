b80170e562083c52cfd5b65ce115511193069c3f
Gabriel Tinoco <gabrielslai@gmail.com>
2021-10-18 14:58:51 -0300

    Added business contacts routing, and routing authentication.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
 let usersController = require('../controllers/users');
 
+function requireAuth(req, res, next) {
+    //Checks if user is logged in.
+    if(!req.isAuthenticated())
+    {
+        res.redirect('/login');
+    }
+    next();
+};
+
 /* GET business contacts page. */
-router.get('/business-contacts', usersController.displayBusinessContactsPage);
+router.get('/business-contacts', requireAuth, usersController.displayBusinessContactsPage);
 
 /* GET/POST edit business contacts page. */
-router.get('/business-contacts/update/:id', usersController.displayUpdatePage);
-router.post('/business-contacts/update/:id', usersController.processUpdatePage);
+router.get('/business-contacts/update/:id', requireAuth, usersController.displayUpdatePage);
+router.post('/business-contacts/update/:id', requireAuth, usersController.processUpdatePage);
 
 module.exports = router;
